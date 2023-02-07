a553fb39ee148323f261ce3ba2917c8e51e6f4c4
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-02-07 18:27:46 +0200

    compled tasks 8-10

--- js/task-08.js
+++ js/task-08.js
@@ -1,4 +1,24 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //деструктуризація змінних email та password з об'єкта form.elements
+  const { email, password } = form.elements;
+
+  if (email.value.trim() === "" || password.value.trim() === "") {
+    alert("Всі поля повинні бути заповнені");
+  } else {
+    const user = {
+      [email.name]: email.value,
+      [password.name]: password.value,
+    };
+
+    console.log(user);
+
+    event.currentTarget.reset();
+  }
+});
 
 // Напиши скрипт управління формою логіна.
 // Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
@@ -6,3 +26,24 @@
 // Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
 // Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
 // Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+
+// const form = document.querySelector(".login-form");
+// console.log(form.elements);
+// form.addEventListener("submit", (event) => {
+// event.preventDefault();
+//   if (form.elements.email.value === "" || form.elements.password.value === "") {
+// alert("Всі поля повинні бути заповнені");
+//   } else {
+// const user = {
+//   console.log(event.currentTarget[0].value);
+//   console.log(form.elements.email.value);
+//   console.log(event.currentTarget[1].value);
+//   console.log(form.elements.password.value);
+// ім'я поля буде ім'ям властивості, а значення поля - значенням властивості
+//   [form.elements[0].name]: form.elements.email.value,
+//   [form.elements[1].name]: form.elements.password.value,
+// };
+// console.log(user);
+// event.currentTarget.reset();
+//   }
+// });
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const btnRef = document.querySelector(".change-color");
+const spanRef = document.querySelector(".color");
+
+btnRef.addEventListener("click", onChangeColor);
+
+function onChangeColor(event) {
+  spanRef.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+//Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку на button.change-color і виводить значення кольору в span.color.
+//Для генерування випадкового кольору використовуй функцію getRandomHexColor.
